d79c8751ad3f7e6889e14a2c68edbb96ab005d40
Rich Chang <rich.miaofu.chang@gmail.com>
2014-09-17 15:51:28 -0700

    Add <SetCardGameViewController>
    Add <SetCardGameViewController>  as a subclass of
    CardGameViewController. It’s designed to control the MVC of Set game
    model.

---
+++ SetCardGameViewController.m
@@ -0,0 +1,18 @@
+//
+//  SetCardGameViewController.m
+//  Matchismo
+//
+//  Created by Rich Chang on 9/17/14.
+//  Copyright (c) 2014 Rich.Chang. All rights reserved.
+//
+
+#import "SetCardGameViewController.h"
+
+@interface SetCardGameViewController ()
+
+@end
+
+@implementation SetCardGameViewController
+
+
+@end
