a7a09d6be7ef5f4ebba11581f20fefaf021831eb
Rich Chang <rich.miaofu.chang@gmail.com>
2014-09-19 18:02:45 -0700

    Improve with attributed string in title of cards.
    - Re-write <titleForCard> to display attributed string in title of
    cards.

--- PlayingCardGameViewController.h
+++ PlayingCardGameViewController.h
@@ -10,4 +10,6 @@
 
 @interface PlayingCardGameViewController : CardGameViewController
 
+- (NSDictionary *)suitColors;
+
 @end
--- PlayingCardGameViewController.m
+++ PlayingCardGameViewController.m
@@ -15,8 +15,42 @@
 
 @implementation PlayingCardGameViewController
 
--(Deck *)createDeck{
+- (Deck *)createDeck{
     return [[PlayingCardDeck alloc]init];
+}
+
+- (NSMutableAttributedString *)titleForCard:(Card *)card{
+    
+    NSMutableAttributedString *title;
+    NSMutableAttributedString *suit;
+    
+    if (card.isChosen){
+        NSRange suitRange;
+        if ([card.contents length] == 3){       // For rank is one unit. (e.g. 1,2..9,J,Q,K)
+            suitRange = NSMakeRange(1, 1);
+        }
+        else if ([card.contents length] == 4){  // For rank is two unit. (e.g. 10)
+            suitRange = NSMakeRange(2, 1);
+        }
+        
+        suit = [[NSMutableAttributedString alloc] initWithString:[card.contents substringWithRange:suitRange]];
+        [suit setAttributes:self.suitColors range:NSMakeRange(0, 1)];
+        
+        title = [[NSMutableAttributedString alloc] initWithString:[card.contents substringWithRange:NSMakeRange(0,1)]]; // init with RANK only.
+        [title appendAttributedString:suit];    // title, which is only RANK, appends with SUIT.
+    }
+    else{
+        title = [[NSMutableAttributedString alloc] initWithString:@""];
+    }
+    
+    return title;
+}
+
+- (NSDictionary *)suitColors{
+    return @{@"♠︎" : [UIColor blackColor],
+             @"♥︎" : [UIColor redColor],
+             @"♦︎" : [UIColor redColor],
+             @"♣︎" : [UIColor blackColor]};
 }
 
 @end
